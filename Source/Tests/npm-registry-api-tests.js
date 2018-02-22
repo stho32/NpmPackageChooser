e43f775060e5c0bf8c98c10f4fe41682e0864fec
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-02-22 21:17:39 +0100

    Grabbing the downloads of the last 7 days now works

--- Source/Tests/npm-registry-api-tests.js
+++ Source/Tests/npm-registry-api-tests.js
@@ -1,8 +1,9 @@
 var chai = require('chai');
 var expect = chai.expect; 
 var npm = require('./../npm-registry-api.js');
+var moment = require("moment");
 
-describe('npm-registry-api', function() {
+describe('npm-registry-api.getPackageInfo', function() {
 
   it('will generate an error message when the package does not exist.', function() {
     npm.getPackageInfo("not-existing", (result) => {
@@ -15,13 +16,15 @@ describe('npm-registry-api', function() {
       expect(result.latestVersion, "Latest version").to.equal("1.0.3");
       expect(result.releaseDate, "Release date and time").to.equal("2017-08-24T03:47:34.385Z");
       expect(result.releaseCount, "Release count").to.equal(36);
-      // Get downloads:
-      // https://api.npmjs.org/downloads/range/2018-01-01:2018-02-01/bcrypt
-      console.dir(result);
     });
   });
-
-  it('moment()')
-
 });
 
+
+describe('npm-registry-api.getPackageDownloads', function() {
+  it('grabs the sum of the downloads of the last 7 days', function() {
+    npm.getPackageDownloads("bcrypt", function(data) {
+      expect(data, "Downloads of the last 7 days").to.be.greaterThan(0);
+    });
+  });
+});
--- Source/npm-registry-api.js
+++ Source/npm-registry-api.js
@@ -1,5 +1,52 @@
 let npmRegistryUrl = "https://registry.npmjs.org/";
 let https = require("https");
+let moment = require("moment");
+
+function getPackageDownloads(packageName, onResultAvailable) {
+    let dayCount = 7;
+
+    let fromDate = moment().startOf("day").add(-dayCount, "day").format("YYYY-MM-DD");
+    let tillDate = moment().startOf("day").add(-1, "days").format("YYYY-MM-DD");
+
+    let url = `https://api.npmjs.org/downloads/range/${fromDate}:${tillDate}` + 
+        `/${packageName}`;
+    
+    https.get(url, response => {
+        if ( response.statusCode == 404 ) {
+            onResultAvailable({
+                    errorMessage: "A package with the name '" + packageName + "' could not be found."
+                });
+            return;
+        }
+
+        let resultText = "";
+    
+        response.on('data', data => {
+            resultText += data.toString();
+        });
+
+        response.on('end', () => {
+            let possibleResult = {};
+            let sumLast7Days = 0;
+            try
+            {
+                possibleResult = JSON.parse(resultText);
+
+                for (let i = 0; i < possibleResult.downloads.length; i++) {
+                    sumLast7Days += possibleResult.downloads[i].downloads;
+                }
+            }
+            catch(e) {
+                console.error("Error parsing json from npm! " + e.message)
+            }
+
+            onResultAvailable(sumLast7Days);
+        });
+    
+    }).on('error', (e) => {
+      console.error(e);
+    });
+}
 
 function getPackageInfo(packageName, onResultAvailable) {
     
@@ -42,4 +89,5 @@ function getPackageInfo(packageName, onResultAvailable) {
 }
 
 module.exports = {};
-module.exports.getPackageInfo = getPackageInfo;
+module.exports.getPackageInfo      = getPackageInfo;
+module.exports.getPackageDownloads = getPackageDownloads;
